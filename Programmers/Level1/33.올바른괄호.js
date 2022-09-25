38127d84ef6927c8fc697fcf75e011125abc32e6
Taesk <qkrxotjr9508@gmail.com>
2022-09-25 13:29:32 +0900

    33

---
+++ Programmers/Level1/33.올바른괄호.js
@@ -0,0 +1,25 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/12909
+
+function solution(s) {
+  var answer = true;
+  s = s.split("");
+  while (true) {
+    const idx1 = s.indexOf("(");
+    const idx2 = s.indexOf(")");
+
+    if (idx2 < idx1 || idx1 == -1 || idx2 == -1) {
+      answer = false;
+      break;
+    }
+
+    s.splice(idx2, 1);
+    s.splice(idx1, 1);
+
+    if (s.join("") == "") {
+      break;
+    }
+  }
+  return answer;
+}
+
+// 런타임 오류
