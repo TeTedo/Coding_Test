e7c2cbe443c5b405a9bfd1595941af055aedd00c
Taesk <qkrxotjr9508@gmail.com>
2022-09-25 13:44:22 +0900

    33

--- Programmers/Level1/33.올바른괄호.js
+++ Programmers/Level1/33.올바른괄호.js
@@ -23,3 +23,25 @@ function solution(s) {
 }
 
 // 런타임 오류
+
+function solution(s) {
+  var answer = true;
+  while (true) {
+    const idx1 = s.indexOf("(");
+    const idx2 = s.indexOf(")");
+
+    if (idx2 < idx1 || idx1 == -1 || idx2 == -1) {
+      answer = false;
+      break;
+    }
+
+    s = "".concat(s.slice(0, idx1), s.slice(idx1 + 1, idx2), s.slice(idx2 + 1));
+
+    if (s == "") {
+      break;
+    }
+  }
+  return answer;
+}
+
+// 런타임오류
