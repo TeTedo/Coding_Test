8a49e289e8df9405bba6703bd41434b2eb312db0
TeTedo <qkrxotjr9508@gmail.com>
2022-10-15 14:52:04 +0900

    삼총사

--- Programmers/Level1/63.삼총사.js
+++ Programmers/Level1/63.삼총사.js
@@ -1,13 +1,13 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/131705
+
 function solution(number) {
   let answer = 0;
   for (let i = 0; i < number.length - 2; i++) {
     for (let k = i + 1; k < number.length - 1; k++) {
-      for (let j = k + 1; k < number.length; j++) {
+      for (let j = k + 1; j < number.length; j++) {
         if (number[i] + number[k] + number[j] == 0) answer += 1;
       }
     }
   }
   return answer;
 }
-
-console.log(solution([-2, 3, 0, 2, -5]));
