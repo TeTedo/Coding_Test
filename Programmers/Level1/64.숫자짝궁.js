254e307d03a9e7326f3f9ad03af4a00cd82d870e
Taesk <qkrxotjr9508@gmail.com>
2022-10-25 23:59:42 +0900

    10.25

--- Programmers/Level1/64.숫자짝궁.js
+++ Programmers/Level1/64.숫자짝궁.js
@@ -14,4 +14,22 @@ function solution(X, Y) {
 }
 
 // 런타임 오류 for문안에서 replace를 불러와서 시간복잡도 O(n^2) 이상이라고 생각
-// 시간 복잡도를 위해 O(n)으로 풀방법을 생각 -> 스택 큐로 풀기
+// 시간 복잡도를 위해 O(n)으로 풀방법을 생각
+
+function solution(X, Y) {
+  //X 요소들 배열에 넣기
+  const obj1 = {};
+  for (let i = 0; i < X.length; i++) {
+    obj1[X[i]] ? (obj1[X[i]] += 1) : (obj1[X[i]] = 1);
+  }
+  //요소들 비교해서 있으면 temp배열에 채우기
+  const temparr = [];
+  for (let j = 0; j < Y.length; j++) {
+    if (obj1[Y[j]]) {
+      obj1[Y[j]] -= 1;
+      temparr.push(Y[j]);
+    }
+  }
+  const answer = temparr.sort((a, b) => b - a).join("");
+  return answer[0] == 0 ? "0" : answer ? answer : "-1";
+}
