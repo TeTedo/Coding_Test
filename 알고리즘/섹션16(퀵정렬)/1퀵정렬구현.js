95648b85694226fc2ba10a155f3b5d3008e9cdf2
Taesk <qkrxotjr9508@gmail.com>
2022-11-17 17:43:12 +0900

    11.17

--- 알고리즘/섹션16(퀵정렬)/1퀵정렬구현.js
+++ 알고리즘/섹션16(퀵정렬)/1퀵정렬구현.js
@@ -1,5 +1,25 @@
-function pivot(arr) {
-  let start = 0;
-  let end = arr.length - 1;
+function pivot(arr, start = 0, end = arr.length - 1) {
+  const swap = (array, i, j) => {
+    [array[i], array[j]] = [array[j], array[i]];
+  };
+
+  let pivot = arr[start]; // 아무값 지정 가능
+  let swapIdx = start;
+  for (let i = start + 1; i <= end; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
+    }
+  }
+  swap(arr, start, swapIdx);
+  return swapIdx;
 }
+const quickSort = (arr, left = 0, right = arr.length - 1) => {
+  if (left < right) {
+    let pivotIdx = pivot(arr, left, right);
+    quickSort(arr, left, pivotIdx - 1);
+    quickSort(arr, pivotIdx + 1, right);
+  }
+  return arr;
+};
 console.log(quickSort([5, 2, 7, 8, 1, 3, 6, 4]));
